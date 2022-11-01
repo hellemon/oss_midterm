eb30d850d846f59a8d9fc700a457b7b76f75f547
hellemon <hellemon@naver.com>
2022-11-01 20:18:00 +0900

    add Are you sure & yes or Yes ... change lower

--- main.py
+++ main.py
@@ -51,9 +51,29 @@ while True:
 
         # check if user wants another calculation
         # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
+        while True:
+            next_calculation = input("Let's do next calculation? (yes/no): ")
+            # Change to Lowercase to Troubleshoot
+            next_calculation = next_calculation.lower()
+            if next_calculation == "no":
+                while True:
+                    True_exit = input("Are you sure? (yes/no): ")
+                    True_exit = True_exit.lower()
+                    if True_exit == "yes":
+                        break
+                    elif True_exit == "no":
+                        break
+                    else:
+                        continue                   
+                break
+            elif next_calculation == "yes":
+                break
+            else:
+                continue
+
+        if True_exit == "yes" and next_calculation == "no":
             break
+            
 
     else:
         print("Invalid Input")
