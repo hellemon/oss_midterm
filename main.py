27720d3bacd57812b8393c3f058563d6dcbfd05c
hellemon <hellemon@naver.com>
2022-11-01 21:08:35 +0900

    remove open_function in main

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ import sys
 import os.path 
 import select_input as si
 
-# make error_log object
-if os.path.isfile("cal_log.txt"):
-    cal_log = open('cal_log.txt', 'a')
-else:
-    cal_log = open('cal_log.txt', 'w')
-if os.path.isfile('error_log.txt'):
-    error_log = open('error_log.txt', 'a')
-else:
-    error_log = open('error_log.txt', 'w')
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
